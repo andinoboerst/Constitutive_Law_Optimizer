0125b38280a47a0d34b6392a98c1557c9baa2140
Andino Boerst <andino.boerst@gmail.com>
2023-02-07 12:11:45 +0100

    commit

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import my_scripts.data as dt
 def main():
     print("Start script")
     restarted = False
-    params = [{"id": "p1", "name": "something", "lower": 2, "upper": 5},
-              {"id": "p2", "name": "something2", "lower": 3, "upper": 20},
-              {"id": "p3", "name": "something3", "lower": 80, "upper": 100}]
+    params = [{"id": "p1", "name": "DENSITY", "lower": 2200, "upper": 2400},
+              {"id": "p2", "name": "YOUNG_MODULUS", "lower": 5500000, "upper": 6500000},
+              {"id": "p3", "name": "POISSON_RATIO", "lower": 0.28, "upper": 0.32}]
     # Step 1: Generate the data
     data = dt.MyData( params, restarted)
     # Step 2: 
--- my_scripts/simulation.py
+++ my_scripts/simulation.py
@@ -1,27 +1,35 @@
 import numpy as np
 import os
 import subprocess
+import json
 
-def run_sims(X, H=np.array([])):
-
-    new_res = np.apply_along_axis(launch_simulation, axis=1, arr=X)
-    if np.size(H)==0:
-        return new_res
-    else:
-        return np.concatenate((H, np.apply_along_axis(launch_simulation, axis=1, arr=X)))
+def run_sims(X):
+    return np.apply_along_axis(launch_simulation, axis=1, arr=X)
     
 
 def launch_simulation(x):
     path = f"{os.getcwd()}/my_files"
 
-    # Here need to modify the ParticleMaterials.json file according to entries in h
+    # Here need to modify the ParticleMaterials.json file according to entries in x
+    with open(f"{path}/ParticleMaterials.json", 'r') as f:
+        json_data = json.load(f)
+
+    json_data["properties"][0]["Material"]["Variables"]["DENSITY"] = x[0]
+    json_data["properties"][0]["Material"]["Variables"]["YOUNG_MODULUS"] = x[1]
+    json_data["properties"][0]["Material"]["Variables"]["POISSON_RATIO"] = x[2]
+
+    with open(f"{path}/ParticleMaterials_new.json", 'w') as f:
+        json.dump(json_data, f, indent=4, separators=(',', ': '))
+
+    input("do smth")
 
     # Run the simulation with the given parameters
     subprocess.run(["/home/andinoboerst/anaconda3/envs/kratos_env/bin/python", f"{path}/MainKratos.py"], cwd=path)
+    os.remove(f"{path}/ParticleMaterials_new.json")
 
-    # Extract the X from the simulation results
-    H = np.array([1, 2, 3])
+    # Extract the h from the simulation results
+    h = np.array([1, 2, 3])
 
     os.remove(f"{path}/falling_sand_ball_results.json")
 
-    return H
+    return h
