108462ee3ac1d1802098cba78f462f0d12153c5f
Andino Boerst <andino.boerst@gmail.com>
2023-02-07 12:51:44 +0100

    commit

--- main.py
+++ main.py
@@ -8,9 +8,9 @@ import my_scripts.data as dt
 def main():
     print("Start script")
     restarted = False
-    params = [{"id": "p1", "name": "DENSITY", "lower": 2200, "upper": 2400},
-              {"id": "p2", "name": "YOUNG_MODULUS", "lower": 5500000, "upper": 6500000},
-              {"id": "p3", "name": "POISSON_RATIO", "lower": 0.28, "upper": 0.32}]
+    params = [{"id": 1, "name": "DENSITY", "lower": 2200, "upper": 2400},
+              {"id": 2, "name": "YOUNG_MODULUS", "lower": 5500000, "upper": 6500000},
+              {"id": 3, "name": "POISSON_RATIO", "lower": 0.28, "upper": 0.32}]
     # Step 1: Generate the data
     data = dt.MyData( params, restarted)
     # Step 2: Train the ML model
